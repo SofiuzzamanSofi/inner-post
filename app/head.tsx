c194c8644fc85d9729a5c180e2b9c8590c40b4c0
Sofiuzzaman Sofi <sofiuzzaman.sf@gmail.com>
2023-04-04 04:51:38 +0600

    setup this project with nextjs typescript tailwindcss

---
+++ app/head.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Head() {
+  return (
+   <>
+   <title>Inner-Talk</title>
+   <meta content='width-device-width, initial-scale=1' name='viewport' />
+   <link rel="" href="" />
+   </>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next'
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default Page
